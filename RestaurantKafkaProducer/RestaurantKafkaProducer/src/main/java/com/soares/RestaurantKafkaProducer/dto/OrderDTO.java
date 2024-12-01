553149f2b0aee371ad18bdd3ff5e9a9112e9e48b
joaov12 <jvfs12th@gmail.com>
2024-12-01 13:52:05 -0300

    build: Cria orderDTO

---
+++ RestaurantKafkaProducer/RestaurantKafkaProducer/src/main/java/com/soares/RestaurantKafkaProducer/dto/OrderDTO.java
@@ -0,0 +1,49 @@
+package com.soares.RestaurantKafkaProducer.dto;
+
+public class OrderDTO {
+    private Long id;
+    private String client;
+    private String cpf;
+    private String product;
+    private double value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
